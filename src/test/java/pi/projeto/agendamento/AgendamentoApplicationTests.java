5c7419d84a346d20e741a078826082bea73f6e41
Anny Freire <anny.yasmim@escolar.ifrn.edu.br>
2024-11-23 17:29:01 -0300

    arquivos iniciais da minha aplicação

---
+++ src/test/java/pi/projeto/agendamento/AgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.projeto.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

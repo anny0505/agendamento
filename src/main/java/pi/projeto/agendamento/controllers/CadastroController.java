2d24c70585b0314b85a082b5fc1afea921a8ef49
Anny Freire <anny.yasmim@escolar.ifrn.edu.br>
2024-11-24 16:21:23 -0300

    Enviando dados do cadastro pra aplicação

--- src/main/java/pi/projeto/agendamento/controllers/CadastroController.java
+++ src/main/java/pi/projeto/agendamento/controllers/CadastroController.java
@@ -1,7 +1,10 @@
 package pi.projeto.agendamento.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pi.projeto.agendamento.models.Cadastro;
 
 @Controller
 public class CadastroController {
@@ -10,6 +13,12 @@ public class CadastroController {
 	public String cadastro() {
 		System.out.println("Chamou o cadastro");
 		return "cadastro";
+	} 
+	
+	@PostMapping("/cadastro")
+	public String adicionarCadastro( Cadastro cadastro) {
+		System.out.println("Cadastro adicionado");
+		return "cadastro-adicionado";
 	}
 
 }

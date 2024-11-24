00735e6b597d149eced2413be7a461060a4fcf2f
Anny Freire <anny.yasmim@escolar.ifrn.edu.br>
2024-11-23 23:39:20 -0300

    Criando formulário de cadastro

--- src/main/java/pi/projeto/agendamento/models/Cadastro.java
+++ src/main/java/pi/projeto/agendamento/models/Cadastro.java
@@ -2,4 +2,41 @@ package pi.projeto.agendamento.models;
 
 public class Cadastro {
 
+	private String nome;
+	private String matricula;
+	private String turma;
+	private String senha;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getTurma() {
+		return turma;
+	}
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
